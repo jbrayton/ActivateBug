20f0dd755c21e77164bd37f69a8f4cd883fbcebd
John Brayton <jbrayton@goldenhillsoftware.com>
2024-05-22 15:04:39 -0400

    Initial Commit

---
+++ ActivateBug/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ActivateBug
+//
+//  Created by John Brayton on 5/22/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
